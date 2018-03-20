706bbf61b02b4728eb0bcd66747a11f23d527775
joantiscar <37572590+joantiscar@users.noreply.github.com>
2018-03-20 08:40:09 +0100

    Començament

---
+++ src/cookieclicker/informacioPartida.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cookieclicker;
+
+/**
+ *
+ * @author joantiscar
+ */
+public class informacioPartida {
+    private double cookies_actuals;
+    private int cursors;
+    private int abueles;
+    private int granjes;
+    private int mines;
+    private int fabriques;
+    private int bancs;
+    private int temples;
+    private int torres_del_mag;
+
+    public double getCookies_actuals() {
+        return cookies_actuals;
+    }
+
+    public void setCookies_actuals(double cookies_actuals) {
+        this.cookies_actuals = cookies_actuals;
+    }
+
+    public int getCursors() {
+        return cursors;
+    }
+
+    public void setCursors(int cursors) {
+        this.cursors = cursors;
+    }
+
+    public int getAbueles() {
+        return abueles;
+    }
+
+    public void setAbueles(int abueles) {
+        this.abueles = abueles;
+    }
+
+    public int getGranjes() {
+        return granjes;
+    }
+
+    public void setGranjes(int granjes) {
+        this.granjes = granjes;
+    }
+
+    public int getMines() {
+        return mines;
+    }
+
+    public void setMines(int mines) {
+        this.mines = mines;
+    }
+
+    public int getFabriques() {
+        return fabriques;
+    }
+
+    public void setFabriques(int fabriques) {
+        this.fabriques = fabriques;
+    }
+
+    public int getBancs() {
+        return bancs;
+    }
+
+    public void setBancs(int bancs) {
+        this.bancs = bancs;
+    }
+
+    public int getTemples() {
+        return temples;
+    }
+
+    public void setTemples(int temples) {
+        this.temples = temples;
+    }
+
+    public int getTorres_del_mag() {
+        return torres_del_mag;
+    }
+
+    public void setTorres_del_mag(int torres_del_mag) {
+        this.torres_del_mag = torres_del_mag;
+    }
+}
