75675ade6b257d429b3bfe73e74745774f8e7e3f
joantiscar <37572590+joantiscar@users.noreply.github.com>
2018-04-24 10:00:24 +0200

    Començament Continuacio Ara pots comprar cursors

--- src/cookieclicker/Board.java
+++ src/cookieclicker/Board.java
@@ -24,7 +24,11 @@ public class Board extends JPanel {
     private boolean inGame;
     private Image big_cookie;
     private Image background;
+    private Image cursor;
+    private Image background_button;
     private JLabel statusbar;
+    private int cps;
+    private int cursors;
     private int cookies_actuals =0;
     
     public Board(JLabel statusbar) {
@@ -50,16 +54,22 @@ public class Board extends JPanel {
     
     private void loadImage() {
         
-        ImageIcon ii = new ImageIcon("src/cookieclicker/img/big_cookie.png");
-        big_cookie = ii.getImage();    
-        ImageIcon io = new ImageIcon("src/cookieclicker/img/background.png");
-        background = io.getImage();
+        ImageIcon iBig_gookie = new ImageIcon("src/cookieclicker/img/big_cookie.png");
+        big_cookie = iBig_gookie.getImage();    
+        ImageIcon iBackground = new ImageIcon("src/cookieclicker/img/background.png");
+        background = iBackground.getImage();
+        ImageIcon iBackground_button = new ImageIcon("src/cookieclicker/img/background_button.png");
+        background_button = iBackground_button.getImage();
+        ImageIcon iCursor = new ImageIcon("src/cookieclicker/img/cursor.png");
+        cursor = iCursor.getImage();
     }
 
     @Override
     public void paintComponent(Graphics g) {
         g.drawImage(background, 0, 0, 800, 600, null);
         g.drawImage(big_cookie, 50, 200, 128, 128, null);
+        g.drawImage(background_button, 500, 200, 200, 130, null);
+        g.drawImage(cursor, 500, 215, 100, 100, null);
         
         
     }
@@ -73,7 +83,13 @@ public class Board extends JPanel {
 
             if(x >= 20 && y >= 200 && x <= 128+20 && y <= 128 + 200){
                 cookies_actuals++;
-            statusbar.setText(Integer.toString(cookies_actuals));
+                refreshStatusBar();
+            }
+            
+            if(x >= 500 && y >= 200 && x <= 500+200 && y <= 200 + 130){
+            cursors++;
+            calcularCps();
+            refreshStatusBar();
             }
           
 
@@ -83,15 +99,21 @@ public class Board extends JPanel {
             
         }
     }
+    public void calcularCps(){
+        cps = cursors*1;
+    }
+    
     class sumarCookies extends TimerTask {
     public void run() {
-       cookies_actuals++;
-       statusbar.setText(Integer.toString(cookies_actuals));
+       cookies_actuals = cookies_actuals + cps;
+       refreshStatusBar();
        
     }
 }
     
-    
+    public void refreshStatusBar(){
+        statusbar.setText("Galletes: " +Integer.toString(cookies_actuals)+"     Cursors: " +Integer.toString(cursors));
+    }
     private void newGame(){
         
         
